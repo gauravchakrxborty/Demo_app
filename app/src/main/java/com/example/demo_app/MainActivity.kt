92d4a5ecfe8e605ed955381f4b7c40a238d22c93
gaurav <gauravchakrxborty@gmail.com>
2024-05-28 06:48:55 +0530

    PaintView.kt, Initialize the Paint Brush

--- app/src/main/java/com/example/demo_app/MainActivity.kt
+++ app/src/main/java/com/example/demo_app/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()//Remove if this is not required later
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/demo_app/PaintView.kt
+++ app/src/main/java/com/example/demo_app/PaintView.kt
@@ -1,4 +1,39 @@
 package com.example.demo_app
 
-class PaintView {
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.View
+import android.view.ViewGroup
+import com.example.demo_app.MainActivity.Companion.paintBrush
+
+class PaintView : View {
+    var params : ViewGroup.LayoutParams? = null
+    companion object{
+        var pathList = ArrayList<Path>()
+        var colorList = ArrayList<Int>()
+        var currentBrush = Color.BLACK;
+    }
+    constructor(context: Context) : this(context, null){
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0){
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    private fun init(){
+        paintBrush.isAntiAlias = true
+        paintBrush.color = currentBrush
+        paintBrush.style = Paint.Style.STROKE
+        paintBrush.strokeJoin = Paint.Join.ROUND
+        paintBrush.strokeWidth = 8f
+
+        params = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT)
+    }
 }
