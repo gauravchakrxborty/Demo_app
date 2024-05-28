a3ddb2816be31a834dd6cc9d47e56fee56a7f365
gaurav <gauravchakrxborty@gmail.com>
2024-05-28 05:56:54 +0530

    MainActivity.kt Complete

--- app/src/main/java/com/example/demo_app/MainActivity.kt
+++ app/src/main/java/com/example/demo_app/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.demo_app
 
+import android.graphics.Paint
+import android.graphics.Path
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        var pathUtils = Path()
+        var paintBrush = Paint()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +25,26 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val redBtn = findViewById<ImageButton>(R.id.redColor)
+        val blueBtn = findViewById<ImageButton>(R.id.blueColor)
+        val blackBtn = findViewById<ImageButton>(R.id.blackColor)
+        val eraser = findViewById<ImageButton>(R.id.whiteColor)
+
+        redBtn.setOnClickListener {
+            Toast.makeText(this,"Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        blueBtn.setOnClickListener {
+            Toast.makeText(this,"Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        blackBtn.setOnClickListener {
+            Toast.makeText(this,"Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        eraser.setOnClickListener {
+            Toast.makeText(this,"Clicked", Toast.LENGTH_SHORT).show()
         }
     }
 }
